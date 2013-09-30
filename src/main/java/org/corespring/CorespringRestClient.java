f5ae9490e03f7927f3c651448cdbdaff3cd3091e
benburton <benjamin.joseph.burton@gmail.com>
2013-09-30 10:17:15 -0400

    CorespringRestClient validates accesstoken

--- src/main/java/org/corespring/CorespringRestClient.java
+++ src/main/java/org/corespring/CorespringRestClient.java
@@ -1,7 +1,21 @@
 package org.corespring;
 
+import org.bson.types.ObjectId;
+
 public class CorespringRestClient {
 
+  private final String accessToken;
 
+  public CorespringRestClient(String accessToken) {
+    validateAccessToken(accessToken);
+    this.accessToken = accessToken;
+  }
+
+  private void validateAccessToken(String accessToken) {
+    if (!ObjectId.isValid(accessToken)) {
+      throw new IllegalArgumentException(
+          new StringBuilder("Access token ").append(accessToken).append(" is not valid").toString());
+    }
+  }
 
 }
--- src/test/java/org/corespring/CorespringRestClientTest.java
+++ src/test/java/org/corespring/CorespringRestClientTest.java
@@ -1,9 +1,34 @@
 package org.corespring;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
-public class CorespringRestClientTest extends TestCase {
+public class CorespringRestClientTest {
 
+  @Test
+  public void testValidAccessTokenDoesNotThrowException() {
 
+    // Success with valid ObjectId
+    try {
+      new CorespringRestClient("52498773a9c98a782be5b739");
+      assertTrue(true);
+    } catch (IllegalArgumentException e) {
+      assertTrue(false);
+    }
+
+  }
+
+  @Test
+  public void testInvalidAccessTokenThrowsException() {
+
+    // Fails with invalid ObjectId
+    try {
+      new CorespringRestClient("abc");
+      assertTrue(false);
+    } catch (IllegalArgumentException e) {
+      assertTrue(true);
+    }
+
+  }
 
 }
