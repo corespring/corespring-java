cfdd2379a8610aa59f29c7e11e45ae4df9e8d32c
benburton <benjamin.joseph.burton@gmail.com>
2013-10-04 15:26:43 -0400

    Use proper id in user agent

--- src/main/java/org/corespring/rest/CorespringRestClient.java
+++ src/main/java/org/corespring/rest/CorespringRestClient.java
@@ -33,7 +33,7 @@ public abstract class CorespringRestClient {
   public static final String API_VERSION = "api/v1";
 
   /** Version of corespring-java client */
-  public static final String VERSION = "1.0-SNAPSHOT";
+  public static final String VERSION = "0.1-SNAPSHOT";
 
   /** Default timeout in milliseconds */
   private static final int CONNECTION_TIMEOUT = 10000;
