54af423763e8653411f79de31d6659dcbc3b8e01
benburton <benjamin.joseph.burton@gmail.com>
2013-11-22 13:51:04 -0500

    v0.3.15

--- src/main/java/org/corespring/rest/CorespringRestClient.java
+++ src/main/java/org/corespring/rest/CorespringRestClient.java
@@ -32,7 +32,7 @@ public abstract class CorespringRestClient {
   public static final String API_VERSION = "api/v1";
 
   /** Version of corespring-java client */
-  public static final String VERSION = "0.4-SNAPSHOT";
+  public static final String VERSION = "0.3.15";
 
   /** Default timeout in milliseconds */
   private static final int CONNECTION_TIMEOUT = 10000;
